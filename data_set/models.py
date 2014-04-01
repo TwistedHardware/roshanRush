3c39ec9e5cddcccbfe74b281e198e438e3a2ded6
Abdullah Alrasheed <twistedhardware@gmail.com>
2014-04-02 00:18:46 +0300

    data_set models

--- data_set/models.py
+++ data_set/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class DateSet(models.Model):
+class DataGroup(models.Model):
     """
     Represents a type of records
     """
@@ -10,8 +10,14 @@ class DateSet(models.Model):
     """
     name = models.CharField(max_length=200)
     
-    
-class RecordType(models.Model):
+    """
+    Methods
+    """
+    def __unicode__(self):
+        return self.name
+
+
+class DataSetType(models.Model):
     """
     Represents a type of records
     """
@@ -20,7 +26,58 @@ class RecordType(models.Model):
     Fields
     """
     name = models.CharField(max_length=200)
+    columns = models.ManyToManyField("Feature", limit_choices_to={'parent': None})
     
+    """
+    Methods
+    """
+    def __unicode__(self):
+        return self.name
+
+
+class DataSet(models.Model):
+    """
+    Represents a type of records
+    """
+    
+    """
+    Fields
+    """
+    name = models.CharField(max_length=200)
+    data_group = models.ForeignKey(DataGroup)
+    
+    """
+    Methods
+    """
+    def __unicode__(self):
+        return self.name
+    
+
+class FeatureType(models.Model):
+    """
+    Represents a type of records
+    """
+    
+    """
+    Fields
+    """
+    name = models.CharField(max_length=200)
+
+
+class Feature(models.Model):
+    """
+    Represents a type of records
+    """
+    
+    """
+    Fields
+    """
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    formula = models.TextField(null=True, blank=True)
+    type = models.ForeignKey(FeatureType)
+    parent = models.ForeignKey("self", null=True, blank=True)
+
 
 class Record(models.Model):
     """
@@ -30,6 +87,107 @@ class Record(models.Model):
     """
     Fields
     """
-    type = models.ForeignKey(RecordType)
+    data_set = models.ForeignKey(DataSet)
     create_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200, null=True, blank=True, default=str(create_date))
     
+    """
+    Methods
+    """
+    def __unicode__(self):
+        return self.name
+    
+
+class DateFeature(models.Model):
+    """
+    Represents a type of records
+    """
+    
+    """
+    Fields
+    """
+    record = models.ForeignKey(Record)
+    feature = models.ForeignKey(Feature)
+    value = models.DateTimeField(null=True, blank=True)
+    
+    """
+    Methods
+    """
+    def __unicode__(self):
+        return "%s: %s" % (self.feature.name, self.value)
+    
+
+class NumberFeature(models.Model):
+    """
+    Represents a type of records
+    """
+    
+    """
+    Fields
+    """
+    record = models.ForeignKey(Record)
+    feature = models.ForeignKey(Feature)
+    value = models.FloatField(null=True, blank=True)
+    
+    """
+    Methods
+    """
+    def __unicode__(self):
+        return "%s: %s" % (self.feature.name, self.value)
+    
+
+class BooleanFeature(models.Model):
+    """
+    Represents a type of records
+    """
+    
+    """
+    Fields
+    """
+    record = models.ForeignKey(Record)
+    feature = models.ForeignKey(Feature)
+    value = models.NullBooleanField(null=True, blank=True)
+    
+    """
+    Methods
+    """
+    def __unicode__(self):
+        return "%s: %s" % (self.feature.name, self.value)
+
+
+class TextFeature(models.Model):
+    """
+    Represents a type of records
+    """
+    
+    """
+    Fields
+    """
+    record = models.ForeignKey(Record)
+    feature = models.ForeignKey(Feature)
+    value = models.TextField(null=True, blank=True)
+    
+    """
+    Methods
+    """
+    def __unicode__(self):
+        return "%s: %s" % (self.feature.name, self.value)
+
+
+class ImageFeature(models.Model):
+    """
+    Represents a type of records
+    """
+    
+    """
+    Fields
+    """
+    record = models.ForeignKey(Record)
+    feature = models.ForeignKey(Feature)
+    value = models.ImageField(upload_to='images', null=True, blank=True)
+    
+    """
+    Methods
+    """
+    def __unicode__(self):
+        return "%s: %s" % (self.feature.name, self.value)
