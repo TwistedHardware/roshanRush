fa9d5d28f08440a7808ea9febe096f21aa52f04a
Abdullah Alrasheed <twistedhardware@gmail.com>
2014-04-04 13:40:52 +0300

    import_data models and admin interface

--- import_data/admin.py
+++ import_data/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+import models
 
-# Register your models here.
+
+class CSVColumnInline(admin.TabularInline):
+    model = model = models.CSVColumn
+    extra = 0
+
+
+class ImportCSVAdmin(admin.ModelAdmin):
+    """
+    Manages admin interface for Import CSV
+    """
+    list_per_page = 100
+    fields = [
+              'file',
+              'data_set',
+              ]
+    list_display = [
+              'file',
+              'data_set',
+                    ]
+    filter_horizontal = []
+    list_filter = []
+    inlines = [CSVColumnInline]
+
+
+admin.site.register(models.ImportCSV, ImportCSVAdmin)
--- import_data/models.py
+++ import_data/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from data_set.models import DataSet,BooleanFeature,DateFeature,ImageFeature,NumberFeature,RecordLinkFeature,TextFeature,Feature
 
-# Create your models here.
+class ImportCSV(models.Model):
+    """
+    Represents a record of importing data from CSV file
+    """
+    
+    """
+    Fields
+    """
+    file = models.FileField(upload_to="csv")
+    create_time = models.DateTimeField(auto_now_add=True)
+    data_set = models.ForeignKey(DataSet, null=True, blank=True)
+    
+    """
+    Methods
+    """
+    def ___unicode__(self):
+        return self.file.name
+
+
+class CSVColumn(models.Model):
+    """
+    Represents a record of importing data from CSV file
+    """
+    
+    """
+    Fields
+    """
+    import_csv = models.ForeignKey(ImportCSV)
+    csv_column = models.CharField(max_length=200)
+    feature = models.ForeignKey(Feature, null=True, blank=True)
+    
+    """
+    Methods
+    """
+    def ___unicode__(self):
+        return self.csv_column
